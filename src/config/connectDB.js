b11df97c10d8339ea120467d331921e87dd755f3
Rudransh <rudransh@example.com>
2026-08-20 17:09:51 +0000

    Initial commit of Damaan Source Code

---
+++ src/config/connectDB.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '92lottery',
+    port: process.env.DB_PORT || 3306
+});
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'admin',
+//     password: 'Dph51mO5qkS8U1k',
+//     database: '92lottery'
+// });
+
+export default connection;
